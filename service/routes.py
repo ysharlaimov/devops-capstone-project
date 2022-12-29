94e399c93459fa8a71ac41033196409baea56c86
yevhenii sharlaimov <yevhenii.sharlaimov@gmail.com>
2022-12-29 13:01:06 -0500

    update accounts

--- service/routes.py
+++ service/routes.py
@@ -90,7 +90,22 @@ def read_account(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_account(account_id):
+    """
+    Updates an Account
+    This endpoint will update an Account based the requested account_id
+    """
+    app.logger.info(f"Request to update an Account with id {account_id}")
+
+    account = Account.find(account_id)
+
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id {account_id} couldn't be found.")
+
+    account.deserialize(request.get_json())
+    account.update()
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
