5fb2d6b491adff667bbd90100ede2cdc64052738
yevhenii sharlaimov <yevhenii.sharlaimov@gmail.com>
2022-12-30 06:43:33 -0500

    flask-talisman

--- service/__init__.py
+++ service/__init__.py
@@ -8,10 +8,18 @@ import sys
 from flask import Flask
 from service import config
 from service.common import log_handlers
+from flask_talisman import Talisman
 
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+
+# Create Talisman
+csp = {
+    'default-src': '\'self\'',
+    'object-src': '\'none\''
+}
+talisman = Talisman(app, content_security_policy=csp)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,6 +12,7 @@ from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
+from service import talisman
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
@@ -33,6 +34,7 @@ class TestAccountService(TestCase):
         app.config["DEBUG"] = False
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
+        talisman.force_https = False
         init_db(app)
 
     @classmethod
